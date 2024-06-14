b726bb427054979a0397561902d2ebd116b9feb7
SuzanneB836 <6035697@mborijnland.nl>
2024-06-14 11:24:02 +0200

    partly adding picture

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['username'])) {
 }
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $picture);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if(isset($_POST['username']) && isset($_POST['password'])) {
